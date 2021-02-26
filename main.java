28ced5d996cc601a17cefa9b0e9848a5fcdfd42f
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-26 17:22:50 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+package hashcode;
+
+public class Main {
+
+    /**
+     * The main driver of the project.
+     * @param args standard arguments
+     */
+    public static void main(final String[] args) {
+    
+        ReadInputFile.getInputFromFile("a.txt");
+        Submission.submit(7);
+    }
+}
